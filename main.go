5554fd12a2bc47585cf4f589314ca00defed4ce6
danielle <danielle@stackery.io>
2019-07-30 10:57:27 -0700

    Seperate out calculateWinner logic and adjust final messaging.

--- main.go
+++ main.go
@@ -5,13 +5,9 @@ import (
 	"golang-rock-paper-scissors/utils"
 )
 
-type Scoreboard struct {
-	User, Computer int
-}
-
 var (
 	playAgain  = "no"
-	totalScore = Scoreboard{}
+	totalScore = map[string]int{"Computer": 0, "User": 0, "Tie": 0, "Rounds": 0}
 )
 
 func main() {
@@ -22,56 +18,10 @@ func main() {
 	fmt.Println("User choice: ", userChoice)
 	fmt.Println("Computer choice: ", computerChoice)
 
-	// Compare answers and declare winner
-	if userChoice == "rock" {
-		if computerChoice == "rock" {
-			fmt.Println("Both picked rock, it's a tie!")
-		}
+	winner := utils.CalculateWinner(computerChoice, userChoice)
+	totalScore[winner]++
+	totalScore["Rounds"]++
 
-		if computerChoice == "paper" {
-			totalScore.Computer++
-			fmt.Println("Paper covers rock...computer wins!")
-		}
-
-		if computerChoice == "scissors" {
-			totalScore.User++
-			fmt.Println("Rock crushes scissors...you win!")
-		}
-	}
-
-	if userChoice == "paper" {
-		if computerChoice == "rock" {
-			totalScore.User++
-			fmt.Println("Paper covers rock...you win!")
-		}
-
-		if computerChoice == "paper" {
-			fmt.Println("Both picked paper, it's a tie!")
-		}
-
-		if computerChoice == "scissors" {
-			totalScore.Computer++
-			fmt.Println("Scissors cuts paper...computer wins!")
-		}
-	}
-
-	if userChoice == "scissors" {
-		if computerChoice == "rock" {
-			totalScore.Computer++
-			fmt.Println("Rock crushes scissors...computer wins!")
-		}
-
-		if computerChoice == "paper" {
-			totalScore.User++
-			fmt.Println("Scissors cuts paper...you win!")
-		}
-
-		if computerChoice == "scissors" {
-			fmt.Println("Both picked scissors...it's a tie!")
-		}
-	}
-
-	// Ask if you'd like to play again
 	playAgain := utils.PlayAgain()
 
 	if playAgain {
@@ -79,7 +29,21 @@ func main() {
 	} else {
 		fmt.Println("Thanks for playing, here's the final results")
 		fmt.Println("--------------------------------------------")
-		fmt.Printf("User: %v\n", totalScore.User)
-		fmt.Printf("Computer: %v\n", totalScore.Computer)
+		fmt.Printf("User: %v\n", totalScore["User"])
+		fmt.Printf("Computer: %v\n", totalScore["Computer"])
+		fmt.Printf("Tie: %v\n", totalScore["Tie"])
+		fmt.Printf("Number of games played: %v\n", totalScore["Rounds"])
+
+		if (totalScore["User"] > totalScore["Computer"]) {
+			fmt.Println("Final result: Congratualations, you won overall!")
+		}
+
+		if (totalScore["User"] < totalScore["Computer"]) {
+			fmt.Println("Final result: The computer did better overall. Better luck next time.")
+		}
+
+		if (totalScore["User"] == totalScore["Computer"]) {
+			fmt.Println("Final result: It was a tie overall.")
+		}
 	}
 }
